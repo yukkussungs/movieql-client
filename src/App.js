afd63a0f1ebad3c354880ed9922a358aea80135f
shin <shin@yasuhironoMacBook-Pro.local>
2019-10-31 19:12:19 +0900

    creat router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import client from './apolloClient';
+import Home from './Home';
+import Detail from './Detail';
 
 function App() {
   return (
-    <div className="App" />
+    <ApolloProvider client={client}>
+      <Router>
+        <Route exact={true} path={"/"} component={Home} />
+        <Route path={"/details/:movieId"} component={Detail} />
+      </Router>
+    </ApolloProvider>
   );
 }
 
